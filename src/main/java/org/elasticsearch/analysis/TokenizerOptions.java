75e0fbfb8a428a7f0ff6bf5e4bcd286d9054a045
netcrazy <ilkyu.choi@gmail.com>
2016-02-13 12:30:56 +0900

    토크나이저 옵션 클래스

--- src/main/java/org/elasticsearch/analysis/TokenizerOptions.java
+++ src/main/java/org/elasticsearch/analysis/TokenizerOptions.java
@@ -3,12 +3,15 @@ package org.elasticsearch.analysis;
 /**
  * 토크나이저 옵션
  * @author 최일규
- * @since  2016 -02 -12
+ * @since  2016-02-12
  *
  */
 public class TokenizerOptions {
 
+    //한영오타에 대한 토큰 추출여부 (hello -> ㅗㄷㅣㅣㅐ, 최일규 -> chldlfrb)
     public final static boolean MISTYPE = false;
+
+    //초성검색을 위한 토큰 추출여부 (최일규 -> ㅊㅇㄱ)
     public final static boolean CHOSUNG = false;
 
     private boolean mistype = MISTYPE;
@@ -17,7 +20,7 @@ public class TokenizerOptions {
     private String name = null;
 
     public static TokenizerOptions create(String name) {
-        return new TokenizerOptions( name);
+        return new TokenizerOptions(name);
     }
 
     private TokenizerOptions(String name) {
@@ -31,13 +34,16 @@ public class TokenizerOptions {
     public boolean isMistype() {
         return mistype;
     }
-    public void setMistype( boolean mistype) {
+
+    public void setMistype(boolean mistype) {
         this. mistype = mistype;
     }
+
     public boolean isChosung() {
         return chosung;
     }
-    public void setChosung( boolean chosung) {
+
+    public void setChosung(boolean chosung) {
         this. chosung = chosung;
     }
 }
