f82991100e2465f690be0f5c8db5669a528198c5
netcrazy <ilkyu.choi@gmail.com>
2016-02-19 23:44:01 +0900

    Update JasoDecomposer

--- src/main/java/org/elasticsearch/analysis/JasoDecomposer.java
+++ src/main/java/org/elasticsearch/analysis/JasoDecomposer.java
@@ -2,117 +2,62 @@ package org.elasticsearch.analysis;
 
 /**
  * 자동완성용 자소분해 (자소분해 with WhiteSpace)
- * @author	최일규
- * @since	2016-02-10
+ * @author  최일규
+ * @since   2016 -02 -10
  */
 public class JasoDecomposer {
 
-	//초성(19자) ㄱ ㄲ ㄴ ㄷ ㄸ ㄹ ㅁ ㅂ ㅃ ㅅ ㅆ ㅇ ㅈ ㅉ ㅊ ㅋ ㅌ ㅍ ㅎ
-	static String[] chosungKor = { "ㄱ", "ㄱㄱ", "ㄴ", "ㄷ", "ㄷㄷ", "ㄹ", "ㅁ", "ㅂ", "ㅂㅂ", "ㅅ", "ㅅㅅ", "ㅇ", "ㅈ", "ㅈㅈ", "ㅊ", "ㅋ", "ㅌ", "ㅍ", "ㅎ" };
-	//중성(21자) ㅏ ㅐ ㅑ ㅒ ㅓ ㅔ ㅕ ㅖ ㅗ ㅘ(9) ㅙ(10) ㅚ(11) ㅛ ㅜ ㅝ(14) ㅞ(15) ㅟ(16) ㅠ ㅡ ㅢ(19) ㅣ
-	static String[] jungsungKor = { "ㅏ", "ㅐ", "ㅑ", "ㅒ", "ㅓ", "ㅔ", "ㅕ", "ㅖ", "ㅗ", "ㅗㅏ", "ㅗㅐ", "ㅗㅣ", "ㅛ", "ㅜ", "ㅜㅓ", "ㅜㅔ", "ㅜㅣ", "ㅠ", "ㅡ", "ㅡㅣ", "ㅣ" };
-	//종성(28자) <없음> ㄱ ㄲ ㄳ(3) ㄴ ㄵ(5) ㄶ(6) ㄷ ㄹ ㄺ(9) ㄻ(10) ㄼ(11) ㄽ(12) ㄾ(13) ㄿ(14) ㅀ(15) ㅁ ㅂ ㅄ(18) ㅅ ㅆ ㅇ ㅈ ㅊ ㅋ ㅌ ㅍ ㅎ
-	static String[] jongsungKor = { " ", "ㄱ", "ㄱㄱ", "ㄱㅅ", "ㄴ", "ㄴㅈ", "ㄴㅎ", "ㄷ", "ㄹ", "ㄹㄱ", "ㄹㅁ", "ㄹㅂ", "ㄹㅅ", "ㄹㅌ", "ㄹㅍ", "ㄹㅎ", "ㅁ", "ㅂ", "ㅂㅅ", "ㅅ", "ㅅㅅ", "ㅇ", "ㅈ", "ㅊ", "ㅋ", "ㅌ", "ㅍ", "ㅎ" };
+    //초성(19자) ㄱ ㄲ ㄴ ㄷ ㄸ ㄹ ㅁ ㅂ ㅃ ㅅ ㅆ ㅇ ㅈ ㅉ ㅊ ㅋ ㅌ ㅍ ㅎ
+    static String[] chosungKor = { "ㄱ", "ㄱㄱ", "ㄴ" , "ㄷ" , "ㄷㄷ" , "ㄹ", "ㅁ" , "ㅂ" , "ㅂㅂ" , "ㅅ" , "ㅅㅅ" , "ㅇ" , "ㅈ" , "ㅈㅈ" , "ㅊ" , "ㅋ" , "ㅌ", "ㅍ" , "ㅎ" };
+    //중성(21자) ㅏ ㅐ ㅑ ㅒ ㅓ ㅔ ㅕ ㅖ ㅗ ㅘ(9) ㅙ(10) ㅚ(11) ㅛ ㅜ ㅝ(14) ㅞ(15) ㅟ(16) ㅠ ㅡ ㅢ(19) ㅣ
+    static String[] jungsungKor = { "ㅏ", "ㅐ", "ㅑ", "ㅒ", "ㅓ", "ㅔ", "ㅕ", "ㅖ", "ㅗ", "ㅗㅏ", "ㅗㅐ" , "ㅗㅣ" , "ㅛ" , "ㅜ" , "ㅜㅓ" , "ㅜㅔ" , "ㅜㅣ", "ㅠ" , "ㅡ" , "ㅡㅣ" , "ㅣ" };
+    //종성(28자) <없음> ㄱ ㄲ ㄳ(3) ㄴ ㄵ(5) ㄶ(6) ㄷ ㄹ ㄺ(9) ㄻ(10) ㄼ(11) ㄽ(12) ㄾ(13) ㄿ(14) ㅀ(15) ㅁ ㅂ ㅄ(18) ㅅ ㅆ ㅇ ㅈ ㅊ ㅋ ㅌ ㅍ ㅎ
+    static String[] jongsungKor = { " ", "ㄱ", "ㄱㄱ", "ㄱㅅ" , "ㄴ" , "ㄴㅈ", "ㄴㅎ" , "ㄷ" , "ㄹ" , "ㄹㄱ" , "ㄹㅁ" , "ㄹㅂ" , "ㄹㅅ" , "ㄹㅌ" , "ㄹㅍ" , "ㄹㅎ" , "ㅁ" , "ㅂ" , "ㅂㅅ" , "ㅅ" , "ㅅㅅ" , "ㅇ" , "ㅈ" , "ㅊ" , "ㅋ" , "ㅌ", "ㅍ" , "ㅎ" };
 
     static String[] chosungEng = { "r", "R", "s", "e", "E", "f", "a", "q", "Q" , "t" , "T" , "d" , "w" , "W" , "c" , "z" , "x" , "v" , "g" };
     static String[] jungsungEng = { "k", "o", "i", "O", "j", "p", "u", "P", "h" , "hk" , "ho" , "hl" , "y" , "n" , "nj" , "np" , "nl" , "b" , "m" , "ml" , "l" };
     static String[] jongsungEng = { "", "r", "R", "rt", "s", "sw", "sg", "e" , "f" , "fr" , "fa" , "fq" , "ft" , "fx" , "fv" , "fg" , "a" , "q" , "qt", "t" , "T" , "d" , "w" , "c" , "z" , "x" , "v" , "g" };
 
-    static String[] mistyping = { "ㅁ", "ㅠ", "ㅊ", "ㅇ", "ㄷ", "ㄹ", "ㅎ", "ㅗ" , "ㅑ" , "ㅓ" , "ㅏ" , "ㅣ" , "ㅡ" , "ㅜ" , "ㅐ" , "ㅔ" , "ㅂ" , "ㄱ" , "ㄴ", "ㅅ" , "ㅕ" , "ㅍ" , "ㅈ" , "ㅌ" , "ㅛ" , "ㅋ" };
+    static String[] mistyping = { "ㅁ", "ㅠ", "ㅊ", "ㅇ", "ㄷ", "ㄹ", "ㅎ", "ㅗ" , "ㅑ" , "ㅓ" , "ㅏ" , "ㅣ" , "ㅡ" , "ㅜ" , "ㅐ" , "ㅔ" , "ㅂ" , "ㄱ" , "ㄴ" , "ㅅ" , "ㅕ" , "ㅍ" , "ㅈ" , "ㅌ" , "ㅛ" , "ㅋ" };
 
-	public String runJasoDecompose(String originStr, TokenizerOptions options) {
-		
-		char[] termBuffer 	= originStr.toCharArray();
-		StringBuffer bufferKor = new StringBuffer();
-        StringBuffer bufferEng = new StringBuffer();
-        StringBuffer bufferChosung = new StringBuffer();
-        StringBuffer bufferMistyping = new StringBuffer();
-        StringBuffer bufferTmp = new StringBuffer();
+    public String runJasoDecompose(String originStr, TokenizerOptions options) {
 
-        boolean jaso = isJaso(originStr);
-        int strLen = originStr.length();
+        if(!originStr.isEmpty()) {
 
-		int cho;
-		int jung;
-		int jong;
-		for(int i = 0; i < termBuffer.length; i++) {
-			char ch = termBuffer[i];
-			
-			//가(AC00)~힣(D7A3) 에 속한 글자면 분해
-			if(ch >= 0xAC00 && ch <= 0xD7A3 && !jaso) {
-                //Unicode 값으로 환산한다.
-                int uniValue = ch - 0xAC00;
+            //공백, lowercase 처리
+            originStr = originStr.replace(" ", "");
+            originStr = originStr.toLowerCase();
 
-                jong = uniValue % 28;                   //종성
-                cho  = ((uniValue - jong) / 28) / 21;   //초성
-                jung = ((uniValue - jong) / 28) % 21;   //중성
+            char[] termBuffer = originStr.toCharArray();
+            StringBuffer korBuffer = new StringBuffer();
+            StringBuffer engBuffer = new StringBuffer();
+            StringBuffer chosungBuffer = new StringBuffer();
+            StringBuffer mistypingBuffer = new StringBuffer();
+            StringBuffer etcBuffer = new StringBuffer();
+            StringBuffer returnBuffer = new StringBuffer();
 
-                //한글초성
-                bufferKor.append(chosungKor[cho]);
+            //첫글자가 한글일때만 초성분해
+            boolean firstCharType = false;
+            if(termBuffer. length > 0)
+                firstCharType = isHangul(Character.toString(termBuffer[0]));
 
-                //한글에 대한 초성만 토큰처리 (일반적으로 색인시 분해함)
-                if(options.isChosung()) {
-                    //초성은 2자이상일때 분해
-                    if(strLen >= 2)
-                        bufferChosung.append(chosungKor[cho]);
-                }
+            //자소포함여부
+            boolean jaso = isJaso(originStr);
+            //한글포함여부
+            boolean hangul = isHangul(originStr);
+            //영문포함여부
+            boolean english = isEnglish(originStr);
 
-                //한글문장에 대한 영문오타처리 (ㄱ -> r)
-                if(options.isMistype()) {
-                    bufferEng.append(chosungEng[cho]);
-                }
+            int strLen = originStr.length();
 
-                //한글중성
-                bufferKor.append(jungsungKor[jung]);
+            int cho;
+            int jung;
+            int jong;
+            for(int i = 0; i < termBuffer. length; i++) {
+                char ch = termBuffer[i];
 
-                //한글문장에 대한 영문오타처리 (ㅏ-> k)
-                if(options.isMistype()) {
-                    bufferEng.append(jungsungEng[jung]);
-                }
-
-                //받침이 있으면
-                if(jong != 0) {
-                    bufferKor.append(jongsungKor[jong]);
-
-                    //한글문장에 대한 영문오타처리 (ㄲ -> R)
-                    if(options.isMistype()) {
-                        bufferEng.append(jongsungEng[jong]);
-                    }
-                }
-			} else {
-                //한글공백에 대한 토큰분리
-                if(ch == ' ') {
-                    bufferKor.append(ch);
-                    bufferChosung.append(ch);
-                }
-
-                //영문처리
-                if(ch >= 0x61 && ch <= 0x7A || ch >= 0x41 && ch <= 0x5A || ch == ' ' ) {
-                    bufferEng.append(ch);
-                }
-
-                //영문문장에 대한 한글오타처리 (hello -> ㅗ디ㅣㅐ)
-                if(options.isMistype()) {
-                    int index;
-                    if(ch >= 0x61 && ch <= 0x7A) {
-                        //소문자
-                        index = (int) ch-97;
-                        bufferMistyping.append(mistyping[index]);
-                    } else if (ch >= 0x41 && ch <= 0x5A) {
-                        //대문자
-                        index = (int) ch-65;
-                        bufferMistyping.append(mistyping[index]);
-                    } else if(ch == ' ') {
-                        //공백에 대한 토큰분리
-                        bufferMistyping.append(ch);
-                    }
-                }
-			}
-
-            //추가적인 예외상황으로 추가 토큰처리 (ㅗㄷㅣㅣㅐ 검색시)
-            if(jaso) {
-                if(ch >= 0xAC00 && ch <= 0xD7A3) {
-
+                //가(AC00)~힣(D7A3) 에 속한 글자면 분해
+                if(ch >= 0xAC00 && ch <= 0xD7A3 && !jaso) {
                     //Unicode 값으로 환산한다.
                     int uniValue = ch - 0xAC00;
 
@@ -120,46 +65,147 @@ public class JasoDecomposer {
                     cho  = ((uniValue - jong) / 28) / 21;   //초성
                     jung = ((uniValue - jong) / 28) % 21;   //중성
 
-                    bufferTmp.append(chosungKor[cho]);
-                    bufferTmp.append(jungsungKor[jung]);
+                    //한글초성
+                    korBuffer.append(chosungKor[cho]);
+
+                    //한글에 대한 초성만 토큰처리 (일반적으로 색인시 분해함)
+                    if(options.isChosung() && firstCharType) {
+                        //초성은 2자이상일때 분해
+                        if(strLen >= 2)
+                            chosungBuffer.append(chosungKor[cho]);
+                    }
+
+                    //한글문장에 대한 영문오타처리 (ㄱ -> r)
+                    if(options.isMistype()) {
+                        engBuffer.append(chosungEng[cho].toLowerCase());
+                    }
+
+                    //한글중성
+                    korBuffer.append(jungsungKor[jung]);
+
+                    //한글문장에 대한 영문오타처리 (ㅏ-> k)
+                    if(options.isMistype()) {
+                        engBuffer.append(jungsungEng[jung].toLowerCase());
+                    }
+
                     //받침이 있으면
                     if(jong != 0) {
-                        bufferTmp.append(jongsungKor[jong]);
+                        korBuffer.append(jongsungKor[jong]);
+
+                        //한글문장에 대한 영문오타처리 (ㄲ -> R)
+                        if(options.isMistype()) {
+                            engBuffer.append(jongsungEng[jong].toLowerCase());
+                        }
+                    }
+                } else {
+
+                    if(options.isMistype()) {
+                        if (!jaso) {
+                            if (hangul) {
+                                korBuffer.append(ch);
+                            }
+                            engBuffer.append(ch);
+                        }
+                    } else {
+                        if (!jaso) {
+                            if (hangul) {
+                                korBuffer.append(ch);
+                            } else {
+                                engBuffer.append(ch);
+                            }
+                        }
+                    }
+
+                    //영문문장에 대한 한글오타처리 (hello -> ㅗ디ㅣㅐ)
+                    if(options.isMistype() && !hangul) {
+                        int index;
+                        if(ch >= 0x61 && ch <= 0x7A) {
+                            //소문자
+                            index = (int) ch-97;
+                            mistypingBuffer.append(mistyping[index]);
+                        } else if (ch >= 0x41 && ch <= 0x5A) {
+                            //대문자
+                            index = (int) ch-65;
+                            mistypingBuffer.append(mistyping[index]);
+                        } else {
+                            if(hangul || english)
+                                mistypingBuffer.append(ch);
+                        }
                     }
                 }
 
-                if(isJaso(Character.toString(ch))) {
-                    //복자음 강제분리
-                    switch(ch) {
-                        case 'ㄲ': bufferTmp.append("ㄱㄱ"); break;
-                        case 'ㄳ': bufferTmp.append("ㄱㅅ"); break;
-                        case 'ㄵ': bufferTmp.append("ㄴㅈ"); break;
-                        case 'ㄶ': bufferTmp.append("ㄴㅎ"); break;
-                        case 'ㄺ': bufferTmp.append("ㄹㄱ"); break;
-                        case 'ㄻ': bufferTmp.append("ㄹㅁ"); break;
-                        case 'ㄼ': bufferTmp.append("ㄹㅂ"); break;
-                        case 'ㄽ': bufferTmp.append("ㄹㅅ"); break;
-                        case 'ㄾ': bufferTmp.append("ㄹㅌ"); break;
-                        case 'ㄿ': bufferTmp.append("ㄹㅍ"); break;
-                        case 'ㅀ': bufferTmp.append("ㄹㅎ"); break;
-                        case 'ㅄ': bufferTmp.append("ㅂㅅ"); break;
-                        default: bufferTmp.append(ch);
-                    }
-                } else {
-                    if(ch == ' ') {
-                        bufferTmp.append(ch);
+                //추가적인 예외상황으로 추가 토큰처리 (ㅗㄷㅣㅣㅐ 검색시)
+                if(jaso) {
+
+                    if(ch >= 0xAC00 && ch <= 0xD7A3) {
+
+                        //Unicode 값으로 환산한다.
+                        int uniValue = ch - 0xAC00;
+
+                        jong = uniValue % 28;                   //종성
+                        cho  = ((uniValue - jong) / 28) / 21;   //초성
+                        jung = ((uniValue - jong) / 28) % 21;   //중성
+
+                        etcBuffer.append(chosungKor[cho]);
+                        etcBuffer.append(jungsungKor[jung]);
+                        //받침이 있으면
+                        if(jong != 0) {
+                            etcBuffer.append(jongsungKor[jong]);
+                        }
+                    } else if(isJaso(Character.toString(ch))) {
+                        //복자음 강제분리
+                        switch(ch) {
+                            case 'ㄲ': etcBuffer.append("ㄱㄱ"); break;
+                            case 'ㄳ': etcBuffer.append("ㄱㅅ"); break;
+                            case 'ㄵ': etcBuffer.append("ㄴㅈ"); break;
+                            case 'ㄶ': etcBuffer.append("ㄴㅎ"); break;
+                            case 'ㄺ': etcBuffer.append("ㄹㄱ"); break;
+                            case 'ㄻ': etcBuffer.append("ㄹㅁ"); break;
+                            case 'ㄼ': etcBuffer.append("ㄹㅂ"); break;
+                            case 'ㄽ': etcBuffer.append("ㄹㅅ"); break;
+                            case 'ㄾ': etcBuffer.append("ㄹㅌ"); break;
+                            case 'ㄿ': etcBuffer.append("ㄹㅍ"); break;
+                            case 'ㅀ': etcBuffer.append("ㄹㅎ"); break;
+                            case 'ㅄ': etcBuffer.append("ㅂㅅ"); break;
+                            default: etcBuffer.append(ch);
+                        }
+                    } else {
+                        etcBuffer.append(ch);
                     }
                 }
             }
-		}
 
-        return String.format("%s %s %s %s %s"
-                , bufferKor.toString()
-                , bufferEng.toString()
-                , bufferMistyping.toString()
-                , bufferChosung.toString()
-                , bufferTmp.toString()).trim();
-	}
+            //결과 조합
+            if(korBuffer.length() > 0) {
+                returnBuffer.append(korBuffer.toString());
+                returnBuffer.append(" ");
+            }
+
+            if(engBuffer.length() > 0) {
+                returnBuffer.append(engBuffer.toString());
+                returnBuffer.append(" ");
+            }
+
+            if(mistypingBuffer.length() > 0) {
+                returnBuffer.append(mistypingBuffer.toString());
+                returnBuffer.append(" ");
+            }
+
+            if(chosungBuffer.length() > 0) {
+                returnBuffer.append(chosungBuffer.toString());
+                returnBuffer.append(" ");
+            }
+
+            if(etcBuffer.length() > 0) {
+                returnBuffer.append(etcBuffer.toString());
+                returnBuffer.append(" ");
+            }
+
+            return returnBuffer.toString().trim();
+        } else {
+            return "";
+        }
+    }
 
     /**
      * 문자열에 한글포함 여부
@@ -167,7 +213,16 @@ public class JasoDecomposer {
      * @return
      */
     private boolean isHangul(String str) {
-        return str.matches(".*[ㄱ-ㅎㅏ-ㅣ가-힣]+.*");
+        return str.matches(".*[ㄱ-ㅎㅏ-ㅣ가-힣]+.*" );
+    }
+
+    /**
+     * 문자열에 영문 여부
+     * @param str
+     * @return
+     */
+    private boolean isEnglish(String str) {
+        return str.matches(".*[a-zA-Z]+.*");
     }
 
     /**
--- src/test/java/org/elasticsearch/analysis/JasoTokenizerTest.java
+++ src/test/java/org/elasticsearch/analysis/JasoTokenizerTest.java
@@ -21,6 +21,7 @@ public class JasoTokenizerTest extends TestCase {
 	public void testTokenizer() throws IOException {
 
         TokenizerOptions options = TokenizerOptions.create("test");
+
         //한영오타에 대한 토큰 추출여부 (hello -> ㅗㄷㅣㅣㅐ, 최일규 -> chldlfrb)
         options.setMistype(true);
 
@@ -28,22 +29,104 @@ public class JasoTokenizerTest extends TestCase {
         options.setChosung(true);
 
         List<TestCaseVO> testCase = new ArrayList<TestCaseVO>();
-        testCase.add(new TestCaseVO("최일규", "ㅊㅗㅣㅇㅣㄹㄱㅠ/chldlfrb/ㅊㅇㄱ"));
-        testCase.add(new TestCaseVO("소녀시대", "ㅅㅗㄴㅕㅅㅣㄷㅐ/thsutleo/ㅅㄴㅅㄷ"));
-        testCase.add(new TestCaseVO("Hello", "Hello/ㅗㄷㅣㅣㅐ"));
-        testCase.add(new TestCaseVO("Hello~", "Hello/ㅗㄷㅣㅣㅐ"));
-        testCase.add(new TestCaseVO("무조건 해피엔딩", "ㅁㅜㅈㅗㄱㅓㄴ/ㅎㅐㅍㅣㅇㅔㄴㄷㅣㅇ/anwhrjs/govldpseld/ㅁㅈㄱ/ㅎㅍㅇㄷ"));
-        testCase.add(new TestCaseVO("소원을 말해봐 genie mixversion##!", "ㅅㅗㅇㅜㅓㄴㅇㅡㄹ/ㅁㅏㄹㅎㅐㅂㅗㅏ/thdnjsdmf/akfgoqhk/genie/mixversion/ㅎㄷㅜㅑㄷ/ㅡㅑㅌㅍㄷㄱㄴㅑㅐㅜ/ㅅㅇㅇ/ㅁㅎㅂ"));
-        testCase.add(new TestCaseVO("hush hush; hush hush", "hush/hush/hush/hush/ㅗㅕㄴㅗ/ㅗㅕㄴㅗ/ㅗㅕㄴㅗ/ㅗㅕㄴㅗ"));
-        testCase.add(new TestCaseVO("라벨:모음곡(거울)제4곡(어릿광대의 아침노래)(서현)", "ㄹㅏㅂㅔㄹㅁㅗㅇㅡㅁㄱㅗㄱㄱㅓㅇㅜㄹㅈㅔㄱㅗㄱㅇㅓㄹㅣㅅㄱㅗㅏㅇㄷㅐㅇㅡㅣ/ㅇㅏㅊㅣㅁㄴㅗㄹㅐㅅㅓㅎㅕㄴ/fkqpfahdmarhrrjdnfwprhrdjfltrhkdeodml/dkclashfotjgus/ㄹㅂㅁㅇㄱㄱㅇㅈㄱㅇㄹㄱㄷㅇ/ㅇㅊㄴㄹㅅㅎ"));
-        testCase.add(new TestCaseVO("ㅗ디ㅣㅐ", "ㅗㄷㅣㅣㅐ")); //hello
-        testCase.add(new TestCaseVO("째깅", "ㅈㅈㅐㄱㅣㅇ/World/ㅈㅈㄱ")); //World
-        testCase.add(new TestCaseVO("ㅣㅐ햣ㄷ초", "ㅣㅐㅎㅑㅅㄷㅊㅗ")); //logitech
-        testCase.add(new TestCaseVO("퍗므ㅑㅜ", "ㅍㅑㅅㅁㅡㅑㅜ")); //vitamin
-        testCase.add(new TestCaseVO("랱", "ㄹㅐㅌ/fox")); //fox
-        testCase.add(new TestCaseVO("ㅅ딛퍄냐ㅐㅜ", "ㅅㄷㅣㄷㅍㅑㄴㅑㅐㅜ")); //television
-        testCase.add(new TestCaseVO("최", "ㅊㅗㅣ/chl")); //television
-        testCase.add(new TestCaseVO("ㅅㄴㅅㄷ", "ㅅㄴㅅㄷ")); //소녀시대 초성검색
+
+        if(options.isMistype() == true && options.isChosung() == true) {
+
+            testCase.add(new TestCaseVO("최일규", "ㅊㅗㅣㅇㅣㄹㄱㅠ/chldlfrb/ㅊㅇㄱ"));
+            testCase.add(new TestCaseVO("소녀시대", "ㅅㅗㄴㅕㅅㅣㄷㅐ/thsutleo/ㅅㄴㅅㄷ"));
+            testCase.add(new TestCaseVO("Hello", "hello/ㅗㄷㅣㅣㅐ"));
+            testCase.add(new TestCaseVO("Hello~", "hello~/ㅗㄷㅣㅣㅐ~"));
+            testCase.add(new TestCaseVO("무조건 해피엔딩", "ㅁㅜㅈㅗㄱㅓㄴㅎㅐㅍㅣㅇㅔㄴㄷㅣㅇ/anwhrjsgovldpseld/ㅁㅈㄱㅎㅍㅇㄷ"));
+            testCase.add(new TestCaseVO("소원을 말해봐 genie mixversion##!", "ㅅㅗㅇㅜㅓㄴㅇㅡㄹㅁㅏㄹㅎㅐㅂㅗㅏgeniemixversion##!/thdnjsdmfakfgoqhkgeniemixversion##!/ㅅㅇㅇㅁㅎㅂ"));
+            testCase.add(new TestCaseVO("hush hush; hush hush", "hushhush;hushhush/ㅗㅕㄴㅗㅗㅕㄴㅗ;ㅗㅕㄴㅗㅗㅕㄴㅗ"));
+            testCase.add(new TestCaseVO("라벨:모음곡(거울)제4곡(어릿광대의 아침노래)(서현)", "ㄹㅏㅂㅔㄹ:ㅁㅗㅇㅡㅁㄱㅗㄱ(ㄱㅓㅇㅜㄹ)ㅈㅔ4ㄱㅗㄱ(ㅇㅓㄹㅣㅅㄱㅗㅏㅇㄷㅐㅇㅡㅣㅇㅏㅊㅣㅁㄴㅗㄹㅐ)(ㅅㅓㅎㅕㄴ)/fkqpf:ahdmarhr(rjdnf)wp4rhr(djfltrhkdeodmldkclashfo)(tjgus)/ㄹㅂㅁㅇㄱㄱㅇㅈㄱㅇㄹㄱㄷㅇㅇㅊㄴㄹㅅㅎ"));
+            testCase.add(new TestCaseVO("ㅗ디ㅣㅐ", "ㅗㄷㅣㅣㅐ")); //hello
+            testCase.add(new TestCaseVO("째깅", "ㅈㅈㅐㄱㅣㅇ/world/ㅈㅈㄱ")); //World
+            testCase.add(new TestCaseVO("ㅣㅐ햣ㄷ초", "ㅣㅐㅎㅑㅅㄷㅊㅗ")); //logitech
+            testCase.add(new TestCaseVO("퍗므ㅑㅜ", "ㅍㅑㅅㅁㅡㅑㅜ")); //vitamin
+            testCase.add(new TestCaseVO("랱", "ㄹㅐㅌ/fox")); //fox
+            testCase.add(new TestCaseVO("ㅅ딛퍄냐ㅐㅜ", "ㅅㄷㅣㄷㅍㅑㄴㅑㅐㅜ")); //television
+            testCase.add(new TestCaseVO("최", "ㅊㅗㅣ/chl")); //television
+            testCase.add(new TestCaseVO("ㅅㄴㅅㄷ", "ㅅㄴㅅㄷ")); //소녀시대 초성검색
+            testCase.add(new TestCaseVO("##%@#$%()*&^%$#@!", "##%@#$%()*&^%$#@!"));
+            testCase.add(new TestCaseVO("2000 최신가요", "2000ㅊㅗㅣㅅㅣㄴㄱㅏㅇㅛ/2000chltlsrkdy"));
+            testCase.add(new TestCaseVO("15&", "15&"));
+            testCase.add(new TestCaseVO("최신가요 1990", "ㅊㅗㅣㅅㅣㄴㄱㅏㅇㅛ1990/chltlsrkdy1990/ㅊㅅㄱㅇ"));
+            testCase.add(new TestCaseVO("최신가요&", "ㅊㅗㅣㅅㅣㄴㄱㅏㅇㅛ&/chltlsrkdy&/ㅊㅅㄱㅇ"));
+
+        } else if(options.isMistype() == true && options.isChosung() == false) {
+
+            testCase.add(new TestCaseVO("최일규", "ㅊㅗㅣㅇㅣㄹㄱㅠ/chldlfrb"));
+            testCase.add(new TestCaseVO("소녀시대", "ㅅㅗㄴㅕㅅㅣㄷㅐ/thsutleo"));
+            testCase.add(new TestCaseVO("Hello", "hello/ㅗㄷㅣㅣㅐ"));
+            testCase.add(new TestCaseVO("Hello~", "hello~/ㅗㄷㅣㅣㅐ~"));
+            testCase.add(new TestCaseVO("무조건 해피엔딩", "ㅁㅜㅈㅗㄱㅓㄴㅎㅐㅍㅣㅇㅔㄴㄷㅣㅇ/anwhrjsgovldpseld"));
+            testCase.add(new TestCaseVO("소원을 말해봐 genie mixversion##!", "ㅅㅗㅇㅜㅓㄴㅇㅡㄹㅁㅏㄹㅎㅐㅂㅗㅏgeniemixversion##!/thdnjsdmfakfgoqhkgeniemixversion##!"));
+            testCase.add(new TestCaseVO("hush hush; hush hush", "hushhush;hushhush/ㅗㅕㄴㅗㅗㅕㄴㅗ;ㅗㅕㄴㅗㅗㅕㄴㅗ"));
+            testCase.add(new TestCaseVO("라벨:모음곡(거울)제4곡(어릿광대의 아침노래)(서현)", "ㄹㅏㅂㅔㄹ:ㅁㅗㅇㅡㅁㄱㅗㄱ(ㄱㅓㅇㅜㄹ)ㅈㅔ4ㄱㅗㄱ(ㅇㅓㄹㅣㅅㄱㅗㅏㅇㄷㅐㅇㅡㅣㅇㅏㅊㅣㅁㄴㅗㄹㅐ)(ㅅㅓㅎㅕㄴ)/fkqpf:ahdmarhr(rjdnf)wp4rhr(djfltrhkdeodmldkclashfo)(tjgus)"));
+            testCase.add(new TestCaseVO("ㅗ디ㅣㅐ", "ㅗㄷㅣㅣㅐ")); //hello
+            testCase.add(new TestCaseVO("째깅", "ㅈㅈㅐㄱㅣㅇ/world")); //World
+            testCase.add(new TestCaseVO("ㅣㅐ햣ㄷ초", "ㅣㅐㅎㅑㅅㄷㅊㅗ")); //logitech
+            testCase.add(new TestCaseVO("퍗므ㅑㅜ", "ㅍㅑㅅㅁㅡㅑㅜ")); //vitamin
+            testCase.add(new TestCaseVO("랱", "ㄹㅐㅌ/fox")); //fox
+            testCase.add(new TestCaseVO("ㅅ딛퍄냐ㅐㅜ", "ㅅㄷㅣㄷㅍㅑㄴㅑㅐㅜ")); //television
+            testCase.add(new TestCaseVO("최", "ㅊㅗㅣ/chl")); //television
+            testCase.add(new TestCaseVO("ㅅㄴㅅㄷ", "ㅅㄴㅅㄷ")); //소녀시대 초성검색
+            testCase.add(new TestCaseVO("##%@#$%()*&^%$#@!", "##%@#$%()*&^%$#@!"));
+            testCase.add(new TestCaseVO("2000 최신가요", "2000ㅊㅗㅣㅅㅣㄴㄱㅏㅇㅛ/2000chltlsrkdy"));
+            testCase.add(new TestCaseVO("15&", "15&"));
+            testCase.add(new TestCaseVO("최신가요 1990", "ㅊㅗㅣㅅㅣㄴㄱㅏㅇㅛ1990/chltlsrkdy1990"));
+            testCase.add(new TestCaseVO("최신가요&", "ㅊㅗㅣㅅㅣㄴㄱㅏㅇㅛ&/chltlsrkdy&"));
+
+        } else if(options.isMistype() == false && options.isChosung() == true) {
+
+            testCase.add(new TestCaseVO("최일규", "ㅊㅗㅣㅇㅣㄹㄱㅠ/ㅊㅇㄱ"));
+            testCase.add(new TestCaseVO("소녀시대", "ㅅㅗㄴㅕㅅㅣㄷㅐ/ㅅㄴㅅㄷ"));
+            testCase.add(new TestCaseVO("Hello", "hello"));
+            testCase.add(new TestCaseVO("Hello~", "hello~"));
+            testCase.add(new TestCaseVO("무조건 해피엔딩", "ㅁㅜㅈㅗㄱㅓㄴㅎㅐㅍㅣㅇㅔㄴㄷㅣㅇ/ㅁㅈㄱㅎㅍㅇㄷ"));
+            testCase.add(new TestCaseVO("소원을 말해봐 genie mixversion##!", "ㅅㅗㅇㅜㅓㄴㅇㅡㄹㅁㅏㄹㅎㅐㅂㅗㅏgeniemixversion##!/ㅅㅇㅇㅁㅎㅂ"));
+            testCase.add(new TestCaseVO("hush hush; hush hush", "hushhush;hushhush"));
+            testCase.add(new TestCaseVO("라벨:모음곡(거울)제4곡(어릿광대의 아침노래)(서현)", "ㄹㅏㅂㅔㄹ:ㅁㅗㅇㅡㅁㄱㅗㄱ(ㄱㅓㅇㅜㄹ)ㅈㅔ4ㄱㅗㄱ(ㅇㅓㄹㅣㅅㄱㅗㅏㅇㄷㅐㅇㅡㅣㅇㅏㅊㅣㅁㄴㅗㄹㅐ)(ㅅㅓㅎㅕㄴ)/ㄹㅂㅁㅇㄱㄱㅇㅈㄱㅇㄹㄱㄷㅇㅇㅊㄴㄹㅅㅎ"));
+            testCase.add(new TestCaseVO("ㅗ디ㅣㅐ", "ㅗㄷㅣㅣㅐ")); //hello
+            testCase.add(new TestCaseVO("째깅", "ㅈㅈㅐㄱㅣㅇ/ㅈㅈㄱ")); //World
+            testCase.add(new TestCaseVO("ㅣㅐ햣ㄷ초", "ㅣㅐㅎㅑㅅㄷㅊㅗ")); //logitech
+            testCase.add(new TestCaseVO("퍗므ㅑㅜ", "ㅍㅑㅅㅁㅡㅑㅜ")); //vitamin
+            testCase.add(new TestCaseVO("랱", "ㄹㅐㅌ")); //fox
+            testCase.add(new TestCaseVO("ㅅ딛퍄냐ㅐㅜ", "ㅅㄷㅣㄷㅍㅑㄴㅑㅐㅜ")); //television
+            testCase.add(new TestCaseVO("최", "ㅊㅗㅣ"));
+            testCase.add(new TestCaseVO("ㅅㄴㅅㄷ", "ㅅㄴㅅㄷ")); //소녀시대 초성검색
+            testCase.add(new TestCaseVO("##%@#$%()*&^%$#@!", "##%@#$%()*&^%$#@!"));
+            testCase.add(new TestCaseVO("2000 최신가요", "2000ㅊㅗㅣㅅㅣㄴㄱㅏㅇㅛ"));
+            testCase.add(new TestCaseVO("15&", "15&"));
+            testCase.add(new TestCaseVO("최신가요 1990", "ㅊㅗㅣㅅㅣㄴㄱㅏㅇㅛ1990/ㅊㅅㄱㅇ"));
+            testCase.add(new TestCaseVO("최신가요&", "ㅊㅗㅣㅅㅣㄴㄱㅏㅇㅛ&/ㅊㅅㄱㅇ"));
+
+        } else if(options.isMistype() == false && options.isChosung() == false) {
+
+            testCase.add(new TestCaseVO("최일규", "ㅊㅗㅣㅇㅣㄹㄱㅠ"));
+            testCase.add(new TestCaseVO("소녀시대", "ㅅㅗㄴㅕㅅㅣㄷㅐ"));
+            testCase.add(new TestCaseVO("Hello", "hello"));
+            testCase.add(new TestCaseVO("Hello~", "hello~"));
+            testCase.add(new TestCaseVO("무조건 해피엔딩", "ㅁㅜㅈㅗㄱㅓㄴㅎㅐㅍㅣㅇㅔㄴㄷㅣㅇ"));
+            testCase.add(new TestCaseVO("소원을 말해봐 genie mixversion##!", "ㅅㅗㅇㅜㅓㄴㅇㅡㄹㅁㅏㄹㅎㅐㅂㅗㅏgeniemixversion##!"));
+            testCase.add(new TestCaseVO("hush hush; hush hush", "hushhush;hushhush"));
+            testCase.add(new TestCaseVO("라벨:모음곡(거울)제4곡(어릿광대의 아침노래)(서현)", "ㄹㅏㅂㅔㄹ:ㅁㅗㅇㅡㅁㄱㅗㄱ(ㄱㅓㅇㅜㄹ)ㅈㅔ4ㄱㅗㄱ(ㅇㅓㄹㅣㅅㄱㅗㅏㅇㄷㅐㅇㅡㅣㅇㅏㅊㅣㅁㄴㅗㄹㅐ)(ㅅㅓㅎㅕㄴ)"));
+            testCase.add(new TestCaseVO("ㅗ디ㅣㅐ", "ㅗㄷㅣㅣㅐ")); //hello
+            testCase.add(new TestCaseVO("째깅", "ㅈㅈㅐㄱㅣㅇ")); //World
+            testCase.add(new TestCaseVO("ㅣㅐ햣ㄷ초", "ㅣㅐㅎㅑㅅㄷㅊㅗ")); //logitech
+            testCase.add(new TestCaseVO("퍗므ㅑㅜ", "ㅍㅑㅅㅁㅡㅑㅜ")); //vitamin
+            testCase.add(new TestCaseVO("랱", "ㄹㅐㅌ")); //fox
+            testCase.add(new TestCaseVO("ㅅ딛퍄냐ㅐㅜ", "ㅅㄷㅣㄷㅍㅑㄴㅑㅐㅜ")); //television
+            testCase.add(new TestCaseVO("최", "ㅊㅗㅣ"));
+            testCase.add(new TestCaseVO("ㅅㄴㅅㄷ", "ㅅㄴㅅㄷ")); //소녀시대 초성검색
+            testCase.add(new TestCaseVO("##%@#$%()*&^%$#@!", "##%@#$%()*&^%$#@!"));
+            testCase.add(new TestCaseVO("2000 최신가요", "2000ㅊㅗㅣㅅㅣㄴㄱㅏㅇㅛ"));
+            testCase.add(new TestCaseVO("15&", "15&"));
+            testCase.add(new TestCaseVO("최신가요 1990", "ㅊㅗㅣㅅㅣㄴㄱㅏㅇㅛ1990"));
+            testCase.add(new TestCaseVO("최신가요&", "ㅊㅗㅣㅅㅣㄴㄱㅏㅇㅛ&"));
+
+        }
 
         for(TestCaseVO vo : testCase) {
 
@@ -66,6 +149,5 @@ public class JasoTokenizerTest extends TestCase {
 
             System.out.println(String.format("%s => %s", vo.getOrigin(), sb.toString()));
         }
-	
 	}	
 }
